e2680009a51a6492ba15eb3f079651eeaacc1918
kevinmeasom <kevin.measom@gmail.com>
2014-12-11 12:17:42 -0700

    added GA tracking

--- js/index.js
+++ js/index.js
@@ -27,6 +27,13 @@ document.addEventListener("deviceready", function(e) {
             
 }, "false");
 
+var gaPlugin;
+
+function onDeviceReady() {
+    gaPlugin = window.plugins.gaPlugin;
+    gaPlugin.init(successHandler, errorHandler, "UA-21302636-1", 10);
+}
+
 var app = {
     // Application Constructor
     initialize: function() {
