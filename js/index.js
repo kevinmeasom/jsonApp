1b519a80df30513220f496fa812846c188d330b6
kevinmeasom <kevin.measom@gmail.com>
2014-12-16 13:48:21 -0700

    updated GA analytics ID to correct account attached to app

--- js/index.js
+++ js/index.js
@@ -28,12 +28,82 @@ document.addEventListener("deviceready", function(e) {
 }, "false");
 
 
-var gaPlugin;
 
-function onDeviceReady() {
-    gaPlugin = window.plugins.gaPlugin;
-    gaPlugin.init(successHandler, errorHandler, "UA-47351417-2", 10);
+/****************************
+********* GA CODE ***********
+*****************************/
+
+
+document.addEventListener("deviceready", init, false);
+var myAnalyticsAccount = "UA-47351417-3";
+ 
+var testDiv;
+ 
+function errorHandler(e) {
+  //Lame - do nothing   
+    alert(e.toString());
 }
+ 
+function init() {
+    console.log("init fired");
+ 
+    testDiv = document.querySelector("#testDiv");
+ 
+    //testDiv.innerHTML += "init fired<br>";
+ 
+    gaPlugin = window.plugins.gaPlugin;
+    gaPlugin.init(gaInit, errorHandler, myAnalyticsAccount, 10);    
+    //testDiv.innerHTML += "Main init<br/>";
+    
+    /***Records a fake event
+    document.querySelector("#testButton").addEventListener("touchend", function(e) {
+ 
+        e.preventDefault();
+        
+        //Track an open event
+        gaPlugin.trackEvent(function() {
+            testDiv.innerHTML += "Button event ok<br/>";
+            
+        }, errorHandler, "Button", "Click", "Main Button", new Date()); 
+ 
+    });
+    ***/
+ 
+    /***Records a fake page load
+    document.querySelector("#testFakePage").addEventListener("touchend", function(e) {
+ 
+        e.preventDefault();
+        
+        //Track an open event
+        gaPlugin.trackPage(function() {
+            testDiv.innerHTML += "Fake Page Event ok<br/>";
+            
+        }, errorHandler, "/fake1.html");
+        
+        
+    });
+    ***/
+    
+}
+ 
+/***
+function gaInit() {
+    testDiv.innerHTML += "GA init<br/>";
+    
+    //Track an open event
+    gaPlugin.trackEvent(function() {
+        testDiv.innerHTML += "Track event ok<br/>";
+        
+    }, errorHandler, "App", "Open", "App", new Date()); 
+}
+***/
+
+/****************************
+******* END GA CODE *********
+*****************************/
+
+
+
 
 var app = {
     // Application Constructor
