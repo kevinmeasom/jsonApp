f01e262c23bc42442b94c8c8fc0081d3e6d723ac
kevinmeasom <kevin.measom@gmail.com>
2015-01-07 11:04:15 -0700

    updated google analytics to include element IDs in event labels

--- js/google-analytics.js
+++ js/google-analytics.js
@@ -1,44 +1,41 @@
-jQuery(document).ready(function($){
+var gaPlugin;
+var pageLink = document.getElementById();
+        
+function initialize() {
+    document.addEventListener("deviceready", onDeviceReady, true);
+}
 
-    var gaPlugin;
-    var thisID = $(this).attr('id');
-            
-    function initialize() {
-        document.addEventListener("deviceready", onDeviceReady, true);
-    }
+function onDeviceReady() {
+    gaPlugin = window.plugins.gaPlugin;
+    gaPlugin.init(nativePluginResultHandler, nativePluginErrorHandler, "UA-57802234-1", 10);
+}
 
-    function onDeviceReady() {
-        gaPlugin = window.plugins.gaPlugin;
-        gaPlugin.init(nativePluginResultHandler, nativePluginErrorHandler, "UA-57802234-1", 10);
-    }
+function nativePluginResultHandler (result) {
+    alert('nativePluginResultHandler - '+result);
+    console.log('nativePluginResultHandler: '+result);
+}
 
-    function nativePluginResultHandler (result) {
-        alert('nativePluginResultHandler - '+result);
-        console.log('nativePluginResultHandler: '+result);
-    }
+function nativePluginErrorHandler (error) {
+    alert('nativePluginErrorHandler - '+error);
+    console.log('nativePluginErrorHandler: '+error);
+}
 
-    function nativePluginErrorHandler (error) {
-        alert('nativePluginErrorHandler - '+error);
-        console.log('nativePluginErrorHandler: '+error);
-    }
+function backTracker() {
+  if(/***localStorage.trackpermission==="trackon" && ***/typeof gaPlugin !== "undefined"){
+    gaPlugin.trackEvent( nativePluginResultHandler, nativePluginErrorHandler, "Button", "Click", document.getElementById(), 1);
+    alert('Button Tracked');
+  }    
+}
 
-    function backTracker() {
-      if(/***localStorage.trackpermission==="trackon" && ***/typeof gaPlugin !== "undefined"){
-        gaPlugin.trackEvent( nativePluginResultHandler, nativePluginErrorHandler, "Button", "Click", thisID, 1);
-        alert('Button Tracked');
-      }    
-    }
-
-    function loadTracker() {
-      if(/***localStorage.trackpermission==="trackon" && ***/typeof gaPlugin !== "undefined"){
-        gaPlugin.trackPage( nativePluginResultHandler, nativePluginErrorHandler, "index.html");
-        alert('Page tracked');
-      }    
-    }
+function loadTracker() {
+  if(/***localStorage.trackpermission==="trackon" && ***/typeof gaPlugin !== "undefined"){
+    gaPlugin.trackPage( nativePluginResultHandler, nativePluginErrorHandler, "index.html");
+    alert('Page tracked');
+  }    
+}
 
 
 
-    function goingAway() {
-        gaPlugin.exit(nativePluginResultHandler, nativePluginErrorHandler);
-    }
-});
+function goingAway() {
+    gaPlugin.exit(nativePluginResultHandler, nativePluginErrorHandler);
+}
