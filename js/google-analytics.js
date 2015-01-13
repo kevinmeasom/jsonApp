9071bdbcda0a5e42a64cd5171120de30d1df942e
kevinmeasom <kevin.measom@gmail.com>
2015-01-13 12:18:21 -0700

    updated GA to track menu ID

--- js/google-analytics.js
+++ js/google-analytics.js
@@ -11,7 +11,7 @@ function onDeviceReady() {
 }
 
 function nativePluginResultHandler (result) {
-    //alert('nativePluginResultHandler - '+result);
+    alert('nativePluginResultHandler - '+result);
     console.log('nativePluginResultHandler: '+result);
 }
 
@@ -21,78 +21,90 @@ function nativePluginErrorHandler (error) {
 }
 
 function homeTracker() {
-  if(/***localStorage.trackpermission==="trackon" && ***/typeof gaPlugin !== "undefined"){
+  if(typeof gaPlugin !== "undefined"){
     gaPlugin.trackPage( nativePluginResultHandler, nativePluginErrorHandler, "index.html");
+    //alert('Button Tracked');
+  }    
+}
+function pageTracker(myID) {
+  if(typeof gaPlugin !== "undefined"){
+    gaPlugin.trackPage( nativePluginResultHandler, nativePluginErrorHandler, myID);
     //alert('Button Tracked');
   }    
 }
 function queryTracker() {
   var thisID = jQuery(this).attr('id');
-  if(/***localStorage.trackpermission==="trackon" && ***/typeof gaPlugin !== "undefined"){
+  if(typeof gaPlugin !== "undefined"){
     gaPlugin.trackPage( nativePluginResultHandler, nativePluginErrorHandler, "#search-page");
     //alert('Button Tracked');
   }    
 }
 function aboutTracker() {
-  if(/***localStorage.trackpermission==="trackon" && ***/typeof gaPlugin !== "undefined"){
+  if(typeof gaPlugin !== "undefined"){
     gaPlugin.trackPage( nativePluginResultHandler, nativePluginErrorHandler, "#about-page");
     //alert('Button Tracked');
   }    
 }
 function resourcesTracker() {
-  if(/***localStorage.trackpermission==="trackon" && ***/typeof gaPlugin !== "undefined"){
+  if(typeof gaPlugin !== "undefined"){
     gaPlugin.trackPage( nativePluginResultHandler, nativePluginErrorHandler, "#resources-page");
     //alert('Button Tracked');
   }    
 }
 function strategyTracker() {
-  if(/***localStorage.trackpermission==="trackon" && ***/typeof gaPlugin !== "undefined"){
+  if(typeof gaPlugin !== "undefined"){
     gaPlugin.trackPage( nativePluginResultHandler, nativePluginErrorHandler, "#strategy-page");
     //alert('Button Tracked');
   }    
 }
 function philosophyTracker() {
-  if(/***localStorage.trackpermission==="trackon" && ***/typeof gaPlugin !== "undefined"){
+  if(typeof gaPlugin !== "undefined"){
     gaPlugin.trackPage( nativePluginResultHandler, nativePluginErrorHandler, "#philosophy-page");
     //alert('Button Tracked');
   }    
 }
 function featuresTracker() {
-  if(/***localStorage.trackpermission==="trackon" && ***/typeof gaPlugin !== "undefined"){
+  if(typeof gaPlugin !== "undefined"){
     gaPlugin.trackPage( nativePluginResultHandler, nativePluginErrorHandler, "#features-page");
     //alert('Button Tracked');
   }    
 }
 function newsTracker() {
-  if(/***localStorage.trackpermission==="trackon" && ***/typeof gaPlugin !== "undefined"){
+  if(typeof gaPlugin !== "undefined"){
     gaPlugin.trackPage( nativePluginResultHandler, nativePluginErrorHandler, "#news-page");
     //alert('Button Tracked');
   }    
 }
 function seoTracker() {
-  if(/***localStorage.trackpermission==="trackon" && ***/typeof gaPlugin !== "undefined"){
+  if(typeof gaPlugin !== "undefined"){
     gaPlugin.trackPage( nativePluginResultHandler, nativePluginErrorHandler, "#seo-page");
     //alert('Button Tracked');
   }    
 }
 function subscribeTracker() {
-  if(/***localStorage.trackpermission==="trackon" && ***/typeof gaPlugin !== "undefined"){
+  if(typeof gaPlugin !== "undefined"){
     gaPlugin.trackPage( nativePluginResultHandler, nativePluginErrorHandler, "#subscribe-page");
     //alert('Button Tracked');
   }    
 }
 function contactTracker() {
-  if(/***localStorage.trackpermission==="trackon" && ***/typeof gaPlugin !== "undefined"){
+  if(typeof gaPlugin !== "undefined"){
     gaPlugin.trackPage( nativePluginResultHandler, nativePluginErrorHandler, "#contact-page");
     //alert('Button Tracked');
   }    
 }
 
 function singleTracker(postNum) {
-  if(/***localStorage.trackpermission==="trackon" && ***/typeof gaPlugin !== "undefined"){
+  if(typeof gaPlugin !== "undefined"){
     gaPlugin.trackPage( nativePluginResultHandler, nativePluginErrorHandler, postNum);
     //alert('Single tracked');
   }    
+}
+
+function loadMore(){
+  if(typeof gaPlugin !== "undefined") {
+    gaPlugin.trackEvent();
+  }
 }
 
 
